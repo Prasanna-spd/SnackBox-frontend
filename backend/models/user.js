4a6e6242fb650badd95858ac0fb4fdcb7afa8601
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-06-22 16:16:46 +0530

    added data models

---
+++ backend/models/user.js
@@ -0,0 +1,31 @@
+// const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+// const bcryptSalt = process.env.BCRYPT_SALT;
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  date: { type: Date, default: Date.now() },
+  token: {
+    type: String,
+    default: "",
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
