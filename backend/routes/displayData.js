9b5c276b0fa38e74556d37d55026f46ed77b686e
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-06-27 21:13:40 +0530

    added frontend

--- backend/routes/displayData.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userController = require("../controllers/userController");
-const { displayData } = userController;
-
-router.post("/foodData", displayData);
-
-module.exports = router;
