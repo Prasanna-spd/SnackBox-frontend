ff65494114f526a90a1bffb1c649e6f9f850844b
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-06-27 10:09:26 +0530

    added Oauth login and created controllers

--- backend/routes/createUser.js
+++ backend/routes/createUser.js
@@ -4,35 +4,38 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const bcryptSalt = process.env.BCRYPT_SALT;
+const userController = require("../controllers/userController");
+const { createUser } = userController;
 
 router.post(
   "/createUser",
   body("email").isEmail(),
   body("name").isLength({ min: 5 }),
   body("password", "incorrect Password").isLength({ min: 5 }),
-  async (req, res) => {
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      return res.status(400).json({ results: result.array() });
-    }
+  createUser
+  // async (req, res) => {
+  //   const result = validationResult(req);
+  //   if (!result.isEmpty()) {
+  //     return res.status(400).json({ results: result.array() });
+  //   }
 
-    const salt = await bcrypt.genSalt(bcryptSalt);
-    let securePass = await bcrypt.hash(req.body.password, salt);
+  //   const salt = await bcrypt.genSalt(bcryptSalt);
+  //   let securePass = await bcrypt.hash(req.body.password, salt);
 
-    try {
-      await User.create({
-        name: req.body.name,
-        password: securePass,
-        email: req.body.email,
-        location: req.body.location,
-      });
+  //   try {
+  //     await User.create({
+  //       name: req.body.name,
+  //       password: securePass,
+  //       email: req.body.email,
+  //       location: req.body.location,
+  //     });
 
-      res.json({ success: true });
-    } catch (error) {
-      console.log(error);
-      res.json({ success: false });
-    }
-  }
+  //     res.json({ success: true });
+  //   } catch (error) {
+  //     console.log(error);
+  //     res.json({ success: false });
+  //   }
+  // }
 );
 
 module.exports = router;
--- backend/routes/loginUser.js
+++ backend/routes/loginUser.js
@@ -4,56 +4,61 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const userController = require("../controllers/userController");
 const jwtSecret = process.env.JWT_SECRET;
+
+const { loginUser } = userController;
 
 router.post(
   "/loginUser",
   body("email").isEmail(),
   body("password", "incorrect Password").isLength({ min: 5 }),
-
-  async (req, res) => {
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      return res.status(400).json({ results: result.array() });
-    }
-    let email = req.body.email;
-
-    try {
-      let userData = await User.findOne({ email });
-      if (!userData) {
-        return res
-          .status(400)
-          .json({ errors: "Try Logging With Proper Credentials" });
-      }
-      const compaPassw = await bcrypt.compare(
-        req.body.password,
-        userData.password
-      );
-      if (!compaPassw) {
-        return res
-          .status(400)
-          .json({ errors: "Try Logging With Proper password" });
-      }
-
-      const data = {
-        user: {
-          id: userData.id,
-        },
-      };
-
-      const token = jwt.sign(data, jwtSecret);
-
-      return res.json({ success: true, authToken: token });
-    } catch (error) {
-      console.log(error);
-      res.json({ success: false });
-    }
-  }
+  loginUser
 );
 
-// router.get("/loginUser", async (req, res) => {
-//   let hack = await User.find();
-//   console.log(hack);
-// });
+//   async (req, res) => {
+//     const result = validationResult(req);
+//     if (!result.isEmpty()) {
+//       return res.status(400).json({ results: result.array() });
+//     }
+//     let email = req.body.email;
+
+//     try {
+//       let userData = await User.findOne({ email });
+//       if (!userData) {
+//         return res
+//           .status(400)
+//           .json({ errors: "Try Logging With Proper Credentials" });
+//       }
+//       const compaPassw = await bcrypt.compare(
+//         req.body.password,
+//         userData.password
+//       );
+//       if (!compaPassw) {
+//         return res
+//           .status(400)
+//           .json({ errors: "Try Logging With Proper password" });
+//       }
+
+//       const data = {
+//         user: {
+//           id: userData.id,
+//         },
+//       };
+
+//       const token = jwt.sign(data, jwtSecret);
+
+//       return res.json({ success: true, authToken: token });
+//     } catch (error) {
+//       console.log(error);
+//       res.json({ success: false });
+//     }
+//   }
+// );
+
+// // router.get("/loginUser", async (req, res) => {
+// //   let hack = await User.find();
+// //   console.log(hack);
+// // });
 
 module.exports = router;
