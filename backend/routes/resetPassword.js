00295265a456d776afbbddfafa9edaa6ad05d24c
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-06-27 20:51:20 +0530

    Remove <folder-or-file> from tracking

--- backend/routes/resetPassword.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
-
-const { forgetPassword } = userController;
-
-router.post("/forgetPassword", forgetPassword);
-
-module.exports = router;
